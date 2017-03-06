7df1f7f9e73580527957f2318bd6599e3c5a7540
Igor Grabarski <igorgrabarski@gmail.com>
2017-03-05 22:16:10 -0500

    Delete Trailer.py

--- Trailer.py
+++
@@ -1,9 +0,0 @@
-class Trailer:
-    def __init__(self):
-        self.movie_id = ''
-        self.trailer_id = ''
-        self.key = ''
-        self.name = ''
-        self.site = ''
-        self.size = 0
-        self.type = ''
